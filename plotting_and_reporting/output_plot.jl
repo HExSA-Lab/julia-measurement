84414a9b522744e0274f8057e81c35b9edbd4f7f
Amal Rizvi <arizvi1@hawk.iit.edu>
2018-03-01 19:12:43 -0600

    updates to output_plot

--- plotting_and_reporting/output_plot.jl
+++ plotting_and_reporting/output_plot.jl
@@ -1,7 +1,8 @@
 #
-# Plot_for_output plots output for .dat files recieved from driver.jl 
-# @fn .dat output filename 
-# @pn .pdf plot filename
+# 	Plot_for_output plots output for .dat 
+#	files recieved from driver.jl 
+# 	@fn:  .dat output filename 
+#  	@pn:  .pdf plot filename
 #
 #
 #using StatsBase
@@ -11,31 +12,34 @@ using StatPlots
 #using Winston
 gr()
 
-function plot_for_output(fn,pn)
+function plot_for_output(fn, pn)
 
 	lats = readdlm(fn, '\t' , UInt64 )
         count  = size(lats)[1]
-        p  = Any
-        x = []
+        xlabels = Array{String}(count)
+	aoa = Any[] 
         
-	for i = 1:count
+	for i = 0:count-1
         
 		el  = 2^i
-                push!(x, el)
+                xlabels[i+1] = "$el"
         
 	end
         
-	for i= 1:count
-        
-		row = lats[i,:]
-                x_axis = convert(Array{Int64},x)
-                y_axis = convert(Array{UInt64,1}, row)
-                p = boxplot!( x_axis, y_axis , ylabel="latency (ns)", xlabel="Process count", outliers=false, marker=(0.5, :blue, stroke(3)), key=false)
+	
+	for i = 1:count
+		push!(aoa, lats[i,:])
         end
+
+	p = boxplot!(xlabels, lats[1], ylabel="latency (ns)", xlabel="Process count", outliers=false, marker=(0.5, :blue, stroke(3)), key=false)
        
-	StatPlots.savefig(p,pn)
+	StatPlots.savefig(p, pn)
 end
 
-plot_for_output("output1.dat","plot1.pdf")
-plot_for_output("output2.dat","plot2.pdf")
+#	Before replotting make sure the names of plot files 
+#	are different or the previous ones are deleted.
+	
+#plot_for_output("output1.dat","plot1.pdf")
+
+#plot_for_output("output2.dat","plot2.pdf")
 ~
