cf13c596e27168ff312ab549f4d6d2aa3ffd0f17
Kyle Hale <aikigaeshi@gmail.com>
2018-03-07 01:28:47 -0600

    add ability to plot CDFs and QQ plots from array data

--- plotting_and_reporting/output_plot.jl
+++ plotting_and_reporting/output_plot.jl
@@ -5,18 +5,20 @@
 #  	@pn:  .pdf plot filename
 #
 #
-#using StatsBase
+using StatsBase
 using GR
+using Distributions
 using StatPlots
 #using Plots
 #using Winston
-gr()
+using Gadfly
+#gr()
 
 function plot_for_output(fn, pn)
 
 	lats = readdlm(fn, '\t' , UInt64 )
-        count  = size(lats)[1]
-        xlabels = Array{String}(count)
+    count  = size(lats)[1]
+    xlabels = Array{String}(count)
 	aoa = Any[] 
         
 	for i = 0:count-1
@@ -36,9 +38,39 @@ function plot_for_output(fn, pn)
 	StatPlots.savefig(p, pn)
 end
 
+# 
+# Creates a CDF plot based on a 1-D array of data
+# points. 
+#
+# example:
+# cdf_from_array_data("lock.dat", "lock.pdf", 4inch, 3inch, "Latency", "CDF")
+#
+function cdf_from_array_data(fn, pn, pw, ph, xlabel, ylabel)
+
+    tmp = readdlm(fn)
+    data = tmp[:, 1]
+    p = Gadfly.plot(ecdf(data), minimum(data), maximum(data), Guide.xlabel(xlabel), Guide.ylabel(ylabel))
+    Gadfly.draw(PDF(pn, pw, ph), p)
+
+end
+
+
+
+#
+# example:
+# qq_from_array_data("lock.dat", "lock.pdf", 4inch, 4inch, "Normal", "Data")
+#
+function qq_from_array_data(fn, pn, pw, ph, xlabel, ylabel)
+
+    tmp = readdlm(fn)
+    data = tmp[:, 1]
+    p = Gadfly.plot(x = rand(Normal(mean(data), 1), length(data)), y = data, Stat.qq, Geom.point, Guide.xlabel(xlabel), Guide.ylabel(ylabel))
+    Gadfly.draw(PDF(pn, pw, ph), p)
+
+end
+
 #	Before replotting make sure the names of plot files 
 #	are different or the previous ones are deleted.
-	
 #plot_for_output("output1.dat","plot1.pdf")
 
 #plot_for_output("output2.dat","plot2.pdf")
