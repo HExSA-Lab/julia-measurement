58d91fe6fcb79b730cbef1a6846c08e16f2f2200
Amal Rizvi <arizvi@dhcp90.merunorth96.iit.edu>
2018-03-07 16:55:16 -0600

    Added semaphore, mutexes and RecursiveSpinlock

--- exp_scripts/single-node/synch.jl
+++ exp_scripts/single-node/synch.jl
@@ -5,16 +5,83 @@
 
 # Reentrant locks
 
-# TODO
+
 function measure_relock_lock(iters, throwout)
+
+    
+    lk = Base.Threads.ReursiveSpinlock()
+    lats = Array{Int64}(iters)
+
+    for i=1:throwout
+        s = time_ns()
+        Base.lock(lk)
+        e = time_ns()
+        Base.unlock(lk)
+        lats[i] = e - s
+    end
+
+    for i=1:iters
+        s = time_ns()
+        Base.lock(lk)
+        e = time_ns()
+        Base.unlock(lk)
+        lats[i] = e - s
+    end
+
+    lats
+
 end
 
 # TODO
 function measure_relock_trylock(iters, throwout)
+    
+    lk = Base.Threads.RecursiveSpinLock()
+    lats = Array{Int64}(iters)
+
+    for i=1:throwout
+        s = time_ns()
+        Base.trylock(lk)
+        e = time_ns()
+        Base.unlock(lk)
+        lats[i] = e - s
+    end
+
+    for i=1:iters
+        s = time_ns()
+        Base.trylock(lk)
+        e = time_ns()
+        Base.unlock(lk)
+        lats[i] = e - s
+    end
+
+    lats
+
 end
 
 # TODO
 function measure_relock_unlock(iters, throwout)
+    
+    lk = Base.Threads.RecuursiveSpinLock()
+    lats = Array{Int64}(iters)
+
+    for i=1:throwout
+        Base.lock(lk)
+        s = time_ns()
+        Base.unlock(lk)
+        e = time_ns()
+        lats[i] = e - s
+    end
+
+    for i=1:iters
+        Base.lock(lk)
+        s = time_ns()
+        Base.unlock(lk)
+        e = time_ns()
+        lats[i] = e - s
+    end
+
+    lats
+
 end
 
 
@@ -22,14 +89,81 @@ end
 
 # TODO
 function measure_mutex_lock(iters, throwout)
+
+    
+    lk = Base.Threads.Mutex()
+    lats = Array{Int64}(iters)
+
+    for i=1:throwout
+        s = time_ns()
+        Base.lock(lk)
+        e = time_ns()
+        Base.unlock(lk)
+        lats[i] = e - s
+    end
+
+    for i=1:iters
+        s = time_ns()
+        Base.lock(lk)
+        e = time_ns()
+        Base.unlock(lk)
+        lats[i] = e - s
+    end
+
+    lats
+
 end
 
 # TODO
 function measure_mutex_trylock(iters, throwout)
+    
+    lk = Base.Threads.Mutex()
+    lats = Array{Int64}(iters)
+
+    for i=1:throwout
+        s = time_ns()
+        Base.trylock(lk)
+        e = time_ns()
+        Base.unlock(lk)
+        lats[i] = e - s
+    end
+
+    for i=1:iters
+        s = time_ns()
+        Base.trylock(lk)
+        e = time_ns()
+        Base.unlock(lk)
+        lats[i] = e - s
+    end
+
+    lats
+
 end
 
 # TODO
 function measure_mutex_unlock(iters, throwout)
+    
+    lk = Base.Threads.Mutex()
+    lats = Array{Int64}(iters)
+
+    for i=1:throwout
+        Base.lock(lk)
+        s = time_ns()
+        Base.unlock(lk)
+        e = time_ns()
+        lats[i] = e - s
+    end
+
+    for i=1:iters
+        Base.lock(lk)
+        s = time_ns()
+        Base.unlock(lk)
+        e = time_ns()
+        lats[i] = e - s
+    end
+
+    lats
+
 end
 
 function measure_spinlock_lock(iters, throwout)
@@ -111,10 +245,44 @@ end
 # Semaphores
 
 # TODO: 
-function measure_sem_acquire(iters, throwout)
+function measure_sem_acquire(size, iters, throwout)
+
+	sem = Base.Semaphore(size)
+    	lats = Array{Int64}(iters)
+	for  i = 1:throwout
+		s = time_ns()
+		Base.acquire(sem)
+		e = time_ns()
+		Base.release(sem)
+	end
+	for  i = 1:iters
+		s = time_ns()
+		Base.acquire(sem)
+		e = time_ns()
+		Base.release(sem)
+		lats[i] = e-s
+	end
+	lats
 end
 
 # TODO: 
 function measure_sem_release(iters, throwout)
+
+	sem = Base.Semaphore(size)
+    	lats = Array{Int64}(iters)
+	for  i = 1:throwout
+		Base.acquire(sem)
+		s = time_ns()
+		Base.release(sem)
+		e = time_ns()
+	end
+	for  i = 1:iters
+		Base.acquire(sem)
+		s = time_ns()
+		Base.release(sem)
+		e = time_ns()
+		lats[i] = e-s
+	end
+	lats
 end
 
