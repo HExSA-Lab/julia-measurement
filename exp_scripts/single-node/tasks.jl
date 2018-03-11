2cfc21bd03f92c6081b48fa6727eb9fafe1b122e
Amal Rizvi <arizvi@Amals-MacBook-Air.local>
2018-03-10 18:59:24 -0600

    lINE 118: Waiting task is not runnable. measure_notify_condition

--- exp_scripts/single-node/tasks.jl
+++ exp_scripts/single-node/tasks.jl
@@ -93,62 +93,47 @@ end
 # the condition doesn't matter, the issue here is we need to
 # coordinate between the tasks using a shared variable. Still
 # figuring out how to do this
-function measure_notify_condition(iters)
-	ncl  =Array{Float64}(iters)
-#= 	#create tasks 
-	
-	tsk1 = Task(f)
 
-	#create a condition 
-	cond  =Condition()
-	schedule(tsk1)
-	for i = 1:throwout 
-        	wait(cond)
-		fac(20)
-		s = time_ns()
-		println(1)
-		notify(cond, all = true, error = false)
-		println(2)
-		e = time_ns()
-	end
-	for i = 1:iters 
-        	wait(cond)
-		fac(20)
-		s = time_ns()
-		notify(cond, all = true, error = false)
-		e = time_ns()
+c = Condition()
+ch = Channel(4)
+function notify_workers()
+	notify(c)
+	s = time_ns()
+	put!(ch,s )
+	println("Notifying ..")
+end
+function am_i_notified()
+	
+	wait(c) 	#unless notified task will be suspended and queued
+	e = time_ns()
+	println("Iam notified")
+	put!(ch,e)
+end
+function measure_notify_condition(throwout, iters)
+	ncl  	  =Array{Float64}(iters)
+	tsk1 = Task(notify_workers) #fires the gun
+	tsk2 = Task(am_i_notified)  #sprints
+
+	for i = 1: throwout
+		schedule(tsk2)	# task 2 is waiting for the gun
+		if istaskdone(tsk2)== false
+			yield()
+		end
+		schedule(tsk1)	# task 1 fires gun - start time 
+			      	# task 1 will hear the bang and sprint 
+		s = take!(ch)	#start time stamp
+		e = take!(ch)   # end time stamp
 		ncl[i] = e-s
 	end
-=#
-	c =  Condition() # what is he condition exactly
-	for i = 1:throwout
-		@async begin
-			wait(c)
-			#notified
-		end
-		@async begin 
-			#do some work, what work though?
-			s = time_ns()
-			notify(c)
-			e = time_ns()	
-			ncl[i] = e- s 
-			#finished
-		end
+	for i = 1: iters
+		schedule(tsk2)	# task 2 is waiting on condition c now 
+		schedule(tsk1)	# task 1 will notify task 2 - start time 
+			      	# task 1 will wake up and be queued
+		s = take!(ch)	#start time stamp
+		e = take!(ch)   # end time stamp
+		ncl[i] = e-s
 	end
-	for i = 1:iters
-		@async begin
-			wait(c)
-			#notified
-		end
-		@async begin 
-			#do some work, what work though?
-			s = time_ns()
-			notify(c)
-			e = time_ns()	
-			ncl[i] = e- s 
-			#finished
-		end
-	end
+	
 	ncl
 end
 
