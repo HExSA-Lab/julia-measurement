236049c85f4a78e35c6f114158eeecaa0c3ffc62
Amal Rizvi <arizvi@dhcp125.merunorth102.iit.edu>
2018-03-12 17:26:17 -0500

    added measure_procs_creation

---
+++ exp_scripts/single-node/procs_creation.jl
@@ -0,0 +1,26 @@
+
+function measure_proc_create_lat(iters, throwout, creations)
+    # array of proc creations per second
+    tpcl = Array{Float64}(iters)
+
+    for i = 1:throwout
+        s = time_ns()
+	for j = 1: creations
+        	addprocs(1)
+        end
+        e = time_ns()
+ 	rmprocs(workers())
+    end
+
+    for i = 1:iters
+        s = time_ns()
+	for j = 1: creations
+        	addprocs(1)
+        end
+        e = time_ns()
+        rmprocs(workers())
+	tpcl[i] = creations*1000000000 / ((e - s))
+    end
+    tpcl
+
+end
