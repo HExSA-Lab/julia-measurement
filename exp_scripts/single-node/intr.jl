0c61cdc7bf2b0f9199af84486cc0e03d536e23af
Kyle C. Hale <khale@cs.iit.edu>
2018-03-13 10:31:09 -0500

    interrupt cleanup + calibration code

--- exp_scripts/single-node/intr.jl
+++ exp_scripts/single-node/intr.jl
@@ -1,3 +1,10 @@
+#
+# This experiment measure the latency to deliver
+# an interrupt signal (exception) to a remote worker process
+# on another core. It is assumed that addprocs will stripe
+# workers across cores, or at least that Linux will be doing
+# the right thing if fork()/clone() is involved under the covers.
+#
 
 addprocs(1)
 
@@ -9,6 +16,13 @@ const work = RemoteChannel(()->Channel{Int}(1))
 const res  = RemoteChannel(()->Channel{Int64}(1))
 
 
+# 
+# Worker function which sleeps, waiting for interrupts
+# to wake it. This is considered as a strange alternative
+# to a blocking take!() on a remote channel for condition notification
+# (we cannot apparently use Julia's condition variables (Condition()) 
+# across processes). 
+#
 @everywhere function wait_for_excp(work, res)
     
     wait_iters = take!(work)
@@ -27,6 +41,43 @@ const res  = RemoteChannel(()->Channel{Int64}(1))
             
 end
 
+
+#
+# This measures the overhead of invoking an interrupt,
+# via the interrupt(n) function call. We will use this
+# to calibrate our other interrupt measurements.
+#
+function calib_int_call(throwout, iters)
+
+    lats = Array{Int64}(iters)
+
+    for i=1:throwout
+        s = time_ns()
+        interrupt()
+        e = time_ns()
+    end
+
+    for i=1:iters
+        s = time_ns()
+        interrupt()
+        e = time_ns()
+        lats[i] = s - e
+    end
+
+    lats
+
+end
+
+
+#
+# Measure the time it takes between right before
+# raising an interrupt and right after a remote worker
+# receives the interrupt. The assumption here is
+# that the timers used in both processes are relatively
+# in sync. We need to subtract out the overhead of the actual
+# interrupt call on the local node to approximate event delivery
+# latency.
+#
 function measure_int_latency(throwout, iters)
 
     lats = Array{Int64}(iters)
@@ -50,8 +101,6 @@ function measure_int_latency(throwout, iters)
         interrupt(2)
 
         e = take!(res)
-            
-        lats[i] = e - s
         
     end
 
@@ -78,5 +127,4 @@ function measure_int_latency(throwout, iters)
     lats
 
 end
-
 
