3933c40ca37791307696a8a22ee4fc07d62c7231
Amal Rizvi <arizvi1@hawk.iit.edu>
2018-06-26 22:38:10 +0000

    Added ssh capabilities

--- measurement_infrastructure/chameleon/load_file.sh
+++ measurement_infrastructure/chameleon/load_file.sh
@@ -5,8 +5,10 @@
 
 #install openstack, vim 
 sudo pip install python-openstackclient
-source openstackrc.sh
+cd /home/cc/julia-measurement/measurement_infrastructure/chameleon
+source openrc.sh
 # Get all IP addresses
+cd /home/cc/julia-measurement/exp_scripts/multi-node
 nova list | grep julia | cut -d '|' -f 7 | cut -d ',' -f 2 >>ipaddr
 # Get all machine names
 nova list | grep julia | cut -d '|' -f 3 >> mchnames
@@ -15,3 +17,13 @@ sudo julia make_hostfile.jl >> myhosts
 # append this information in etc/host
 sudo su
 cat myhosts>>/etc/hosts 
+#SSH into all machines
+ssh-keygen -t rsa
+filename=ipaddr
+while read -r ipaddr
+do
+	name="$mchnames"
+	ssh-copy-id $name
+	eval 'ssh-agent'
+	ssh-add ~/.ssh/id_rsa 
+done < "$filename"
