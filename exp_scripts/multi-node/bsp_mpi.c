fb81b4610f40a03f23c77745b56b48c98a8c9388
Amal Rizvi <arizvi1@hawk.iit.edu>
2018-07-24 12:58:32 -0500

    added missing ping pong experiment

--- exp_scripts/multi-node/bsp_mpi.c
+++ exp_scripts/multi-node/bsp_mpi.c
@@ -179,6 +179,76 @@ static void do_comms(struct bsp_type *a)
    
 }
 
+
+#define MIN_PING_PONG_SIZE 8 // in bytes
+#define MAX_PING_PONG_SIZE (1024*1024) // up to 1MB
+
+static void do_ping_pong(struct bsp_type *a)
+{
+    int i;
+    FILE *fs = NULL;
+    struct timespec start;
+    struct timespec end;
+    int tag = 10;
+
+    /* bi-directional test, only two ranks supported */
+    int ping = 0;
+    int pong = 1;
+
+    for (i = MIN_PING_PONG_SIZE; i <= MAX_PING_PONG_SIZE; i *= 2) {
+
+        unsigned char *arr = malloc(i);
+
+        if (!arr) {
+            fprintf(stderr, "Could not allocate array in %s\n", __func__);
+            return;
+        }
+
+        /* start timer */
+        if (a->rank == ping) {
+            char filename[sizeof "comm_size_c_16.dat"];
+            sprintf(filename,"comm_size_c_%d.dat", i);
+            fs = fopen(filename,"a");
+              clock_gettime(CLOCK_REALTIME, &start);
+          }
+
+        /* PING */
+        if (a->rank == ping) {
+            if (MPI_Send(arr, i, MPI_BYTE, pong, tag, a->comm_w) != MPI_SUCCESS) {
+                fprintf(stderr, "MPI_Send (ping stage) unsuccessful\n");
+            }
+        } else if (a->rank == pong) {
+            if (MPI_Recv(arr, i, MPI_BYTE, ping, tag, a->comm_w, MPI_STATUS_IGNORE) != MPI_SUCCESS) {
+                fprintf(stderr, "MPI_Recv (ping stage) unsuccessful\n");
+            }
+        }
+
+        /* PONG */
+        if (a->rank == pong) {
+            if (MPI_Send(arr, i, MPI_BYTE, ping, tag, a->comm_w) != MPI_SUCCESS) {
+                fprintf(stderr, "MPI_Send (pong stage) unsuccessful\n");
+            }
+        } else if (a->rank == ping) {
+            if (MPI_Recv(arr, i, MPI_BYTE, pong, tag, a->comm_w, MPI_STATUS_IGNORE) != MPI_SUCCESS) {
+                fprintf(stderr, "MPI_Recv (pong stage) unsuccessful\n");
+            }
+        }
+
+        if (a->rank == ping) {
+              clock_gettime(CLOCK_REALTIME, &end);
+              long s_ns = start.tv_sec*1000000000 + start.tv_nsec;
+              long e_ns = end.tv_sec*1000000000 + end.tv_nsec;
+              fprintf(fs,"%lu\n", e_ns - s_ns);
+              fclose(fs);
+          }
+
+        /* synch up before trial for next buffer size */
+        MPI_Barrier(a->comm_w);
+      }
+    MPI_Barrier(a->comm_w);
+
+
+  }
 static void do_it(int iters, int elements, int flops, int reads, int writes, int comms)
 {
     
@@ -196,6 +266,9 @@ static void do_it(int iters, int elements, int flops, int reads, int writes, int
     for (j =0; j<iters;j++) {
         do_compute(&a);
         do_comms(&a);
+        if (size==2){
+            do_ping_pong(&a);
+        }
     }
 }
 int 
