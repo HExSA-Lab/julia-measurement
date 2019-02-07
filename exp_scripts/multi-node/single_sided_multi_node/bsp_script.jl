4af4356d52f07bd375a6941f1c23dfd6dc0472ab
Amal Rizvi <amalrizv@gmail.com>
2019-02-07 00:06:28 +0000

    Adding single sided communication package

---
+++ exp_scripts/multi-node/single_sided_multi_node/bsp_script.jl
@@ -0,0 +1,16 @@
+import MPI
+#= FOR JULIA VERSION 0.7 OR HIGHER
+using Distributed
+Distributed.@everywhere include("bsp_julia_mpi.jl")
+=#
+@everywhere include("bsp_julia_mpi.jl")
+
+    iters = 100
+    elements = 10
+    flops = 1000000
+    reads = 5000000
+    writes = 5000000
+    comms = 10000
+#    doit(iters,elements, flops, reads, writes,comms)    
+    doit_mpi(iters,elements, flops, reads, writes,comms)    
+    rmprocs(workers())
