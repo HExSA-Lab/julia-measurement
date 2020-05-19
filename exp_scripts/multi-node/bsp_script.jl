29fda0ad224bd377dd09e48bae5ee6a713d49bc4
Amal Rizvi <amalrizv@gmail.com>
2020-05-19 06:21:49 +0000

    more corrections

--- exp_scripts/multi-node/bsp_script.jl
+++ exp_scripts/multi-node/bsp_script.jl
@@ -1,8 +1,6 @@
 using MPI
-#FOR JULIA VERSION 0.7 OR HIGHER
 using Distributed
-Distributed.@everywhere include("bsp_julia_mpi.jl")
-#@everywhere include("bsp_julia_mpi.jl")
+@everywhere include("bsp_julia_mpi.jl")
 
     iters = 100
     elements = 10
@@ -10,6 +8,5 @@ Distributed.@everywhere include("bsp_julia_mpi.jl")
     reads = 5000000
     writes = 500000
     comms = 10000
-#    doit(iters,elements, flops, reads, writes,comms)    
     doit_mpi(iters,elements, flops, reads, writes,comms)    
     rmprocs(workers())
